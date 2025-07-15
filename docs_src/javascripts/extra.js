c509a3c2d841320976e4f588b2583e299f709a9a
Dominik Morse <aenszguhelem@gmail.com>
2025-07-15 12:22:01 +0200

    Modified image modal
    opening is now smoother

--- docs_src/javascripts/extra.js
+++ docs_src/javascripts/extra.js
@@ -14,33 +14,40 @@ document.addEventListener("DOMContentLoaded", () => {
     caption.className = "modal-caption";
     modal.appendChild(caption);
     document.body.appendChild(modal);
+
     function _blockScroll(e) { e.preventDefault() }
+
     function _blockNavKeys(e) {
         const blockKeys = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", "PageUp", "PageDown", "Home", "End", "Tab", " "];
         if (blockKeys.includes(e.key)) e.preventDefault()
     }
+
     function enableModalInputLock() {
         document.body.style.overflow = "hidden";
         window.addEventListener("wheel", _blockScroll, { passive: false });
         window.addEventListener("touchmove", _blockScroll, { passive: false });
         document.addEventListener("keydown", _blockNavKeys, true);
     }
+
     function disableModalInputLock() {
         document.body.style.overflow = "";
         window.removeEventListener("wheel", _blockScroll, { passive: false });
         window.removeEventListener("touchmove", _blockScroll, { passive: false });
         document.removeEventListener("keydown", _blockNavKeys, true);
     }
+
     function openModal(src, alt) {
-        modal.style.display = "flex";
         modalImg.src = src;
         caption.textContent = alt || "";
+        modal.classList.add("open");
         enableModalInputLock();
     }
+
     function closeModal() {
-        modal.style.display = "none";
+        modal.classList.remove("open");
         disableModalInputLock();
     }
+
     closeBtn.addEventListener("click", closeModal);
     modal.addEventListener("click", e => { if (e.target === modal) closeModal() });
     document.querySelectorAll("img.preview").forEach(img => {
@@ -54,7 +61,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll('.md-tag').forEach(span => {
         const tag = span.textContent.toLowerCase();
         const link = document.createElement('a');
-         link.href = `${window.location.origin}/docs/tags/#tag:${tag}`;
+        link.href = `${window.location.origin}/docs/tags/#tag:${tag}`;
         link.className = span.className + ' md-tag-icon';
         link.innerHTML = span.innerHTML;
         span.replaceWith(link);
